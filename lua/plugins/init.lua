9c034fbe2b7dea53fe7a25aeebca717adb074018
Fabio Montefuscolo <fabio.montefuscolo@gmail.com>
2024-12-07 16:00:05 +0100

    feat: copilot plugin

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -18,6 +18,16 @@ return {
     opts = require "configs.nvim-tree",
   },
 
+
+  {
+    "github/copilot.vim",
+    event = "InsertEnter",
+    config = function()
+      vim.g.copilot_no_tab_map = true
+      vim.g.copilot_assume_mapped = true
+    end,
+    lazy = true,
+  },
   -- {
   -- 	"nvim-treesitter/nvim-treesitter",
   -- 	opts = {
