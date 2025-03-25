3222d0d9fbb5257157503fe30254e48c5230665d
Fabio Montefuscolo <fabio.montefuscolo@gmail.com>
2025-03-25 14:46:00 +0100

    feat: installed avanti plugin

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -3,7 +3,8 @@ return {
     require "plugins.nvim-lspconfig",
     require "plugins.nvim-tree",
     require "plugins.git-blame",
-    require "plugins.copilot",
+    -- require "plugins.copilot",
     require "plugins.vim-sendtowindow",
+    require "plugins.avanti",
     -- require "plugins.nvim-treesitter",
 }
