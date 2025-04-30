e4c8e87ebac77fbea6548d2f7821943c39b248d2
Fabio Montefuscolo <fabio.montefuscolo@gmail.com>
2025-04-30 16:14:40 +0200

    fix: missing commands removed by nvchad team

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -1,11 +1,34 @@
 -- load defaults i.e lua_lsp
 require("nvchad.configs.lspconfig").defaults()
-
+local map = vim.keymap.set
 local lspconfig = require "lspconfig"
 
 -- EXAMPLE
 local servers = { "html", "cssls", "dartls" }
 local nvlsp = require "nvchad.configs.lspconfig"
+
+nvlsp.on_attach = function(_, bufnr)
+  local function opts(desc)
+    return { buffer = bufnr, desc = "LSP " .. desc }
+  end
+
+  map({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, opts "Code action")
+  map("n", "<leader>sh", vim.lsp.buf.signature_help, opts "Show signature help")
+  map("n", "gi", vim.lsp.buf.implementation, opts "Go to implementation")
+  map("n", "gr", vim.lsp.buf.references, opts "List references")
+
+  map("n", "gD", vim.lsp.buf.declaration, opts "Go to declaration")
+  map("n", "gd", vim.lsp.buf.definition, opts "Go to definition")
+  map("n", "<leader>wa", vim.lsp.buf.add_workspace_folder, opts "Add workspace folder")
+  map("n", "<leader>wr", vim.lsp.buf.remove_workspace_folder, opts "Remove workspace folder")
+
+  map("n", "<leader>wl", function()
+    print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+  end, opts "List workspace folders")
+
+  map("n", "<leader>D", vim.lsp.buf.type_definition, opts "Go to type definition")
+  map("n", "<leader>ra", require "nvchad.lsp.renamer", opts "NvRenamer")
+end
 
 -- lsps with default config
 for _, lsp in ipairs(servers) do
