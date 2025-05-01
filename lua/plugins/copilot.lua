135acf003de3d92beea48df619d71171f9e6a795
Fabio Montefuscolo <fabio.montefuscolo@gmail.com>
2025-05-01 10:04:22 +0200

    fix: unused or redundant plugins

--- lua/plugins/copilot.lua
+++
@@ -1,9 +0,0 @@
-return {
-    "github/copilot.vim",
-    event = "InsertEnter",
-    config = function()
-        vim.g.copilot_no_tab_map = true
-        vim.g.copilot_assume_mapped = true
-    end,
-    lazy = true,
-}
--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,10 +1,2 @@
 return {
-    require "plugins.conform",
-    require "plugins.nvim-lspconfig",
-    require "plugins.nvim-tree",
-    require "plugins.git-blame",
-    -- require "plugins.copilot",
-    require "plugins.vim-sendtowindow",
-    require "plugins.avanti",
-    -- require "plugins.nvim-treesitter",
 }
