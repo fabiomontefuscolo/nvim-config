6f07875264bad29ba738cccf7323401a9bef8953
Fabio Montefuscolo <fabio.montefuscolo@gmail.com>
2025-05-24 17:53:40 +0200

    fix: missing terminal shortcuts

--- lua/plugins/toggleterm.lua
+++ lua/plugins/toggleterm.lua
@@ -6,4 +6,9 @@ return {
     size = 10,
     open_mapping = [[<c-\>]],
   },
+  keys = {
+    { '<leader>tf', '<cmd>ToggleTerm direction=float<cr>', desc = 'Toggle floating terminal' },
+    { '<leader>th', '<cmd>ToggleTerm direction=horizontal<cr>', desc = 'Toggle horizontal terminal' },
+    { '<leader>tv', '<cmd>ToggleTerm direction=vertical size=60<cr>', desc = 'Toggle vertical terminal' },
+  },
 }
