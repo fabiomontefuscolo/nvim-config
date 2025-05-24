ff2c899fd55c9665c060c706b110e802c2f5066c
Fabio Montefuscolo <fabio.montefuscolo@gmail.com>
2025-05-24 18:13:29 +0200

    chore: adjust shortcuts again

--- lua/plugins/toggleterm.lua
+++ lua/plugins/toggleterm.lua
@@ -1,3 +1,11 @@
+local trim_spaces = true
+
+vim.keymap.set('v', '<space>l', function()
+  -- require("toggleterm").send_lines_to_terminal("single_line", trim_spaces, { args = vim.v.count })
+  -- require("toggleterm").send_lines_to_terminal("visual_lines", trim_spaces, { args = vim.v.count })
+  require('toggleterm').send_lines_to_terminal('visual_selection', trim_spaces, { args = vim.v.count })
+end)
+
 return {
   'akinsho/toggleterm.nvim',
   version = '*',
@@ -7,6 +15,7 @@ return {
     open_mapping = [[<c-\>]],
   },
   keys = {
+    { '<leader>tt', '<cmd>ToggleTerm<cr>', desc = 'Toggle terminal' },
     { '<leader>tf', '<cmd>ToggleTerm direction=float<cr>', desc = 'Toggle floating terminal' },
     { '<leader>th', '<cmd>ToggleTerm direction=horizontal<cr>', desc = 'Toggle horizontal terminal' },
     { '<leader>tv', '<cmd>ToggleTerm direction=vertical size=60<cr>', desc = 'Toggle vertical terminal' },
