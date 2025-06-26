7751c8bc29aac7bf7225a6611b50fcd63b53c72b
Fabio Montefuscolo <200063206+fabiomontefuscoloprotolabs@users.noreply.github.com>
2025-06-26 18:20:17 +0200

    fix: blink x copilot integration

--- lua/plugins/blink.lua
+++ lua/plugins/blink.lua
@@ -4,26 +4,12 @@ return {
   version = '1.*',
   dependencies = {
     'Kaiser-Yang/blink-cmp-avante',
+    'giuxtaposition/blink-cmp-copilot',
+    'folke/lazydev.nvim',
     {
       'L3MON4D3/LuaSnip',
       version = '2.*',
-      build = (function()
-        if vim.fn.has 'win32' == 1 or vim.fn.executable 'make' == 0 then
-          return
-        end
-        return 'make install_jsregexp'
-      end)(),
-      dependencies = {
-        {
-          'rafamadriz/friendly-snippets',
-          config = function()
-            require('luasnip.loaders.from_vscode').lazy_load()
-          end,
-        },
-      },
-      opts = {},
     },
-    'folke/lazydev.nvim',
   },
   opts = {
     keymap = {
@@ -39,9 +25,15 @@ return {
     },
 
     sources = {
-      default = { 'lsp', 'path', 'snippets', 'lazydev' },
+      default = { 'lsp', 'path', 'copilot', 'snippets', 'buffer', 'lazydev' },
       providers = {
         lazydev = { module = 'lazydev.integrations.blink', score_offset = 100 },
+        copilot = {
+          name = 'copilot',
+          module = 'blink-cmp-copilot',
+          score_offset = 100,
+          async = true,
+        },
       },
     },
 
