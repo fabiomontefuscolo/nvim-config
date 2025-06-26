4e1bc878b4928732847cbfba5b2543881a6bf8e9
Fabio Montefuscolo <200063206+fabiomontefuscoloprotolabs@users.noreply.github.com>
2025-06-26 22:10:54 +0200

    fix: live grep not working in .github folders

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -23,6 +23,18 @@ return {
           require('telescope.themes').get_dropdown(),
         },
       },
+      pickers = {
+        live_grep = {
+          file_ignore_patterns = { 'node_modules/', '.git/', '.venv/' },
+          additional_args = function(_)
+            return { '--hidden' }
+          end,
+        },
+        find_files = {
+          file_ignore_patterns = { 'node_modules/', '.git/', '.venv/' },
+          hidden = true,
+        },
+      },
     }
 
     pcall(require('telescope').load_extension, 'fzf')
