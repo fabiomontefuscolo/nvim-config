4fad3a828c5adf7ae149b604a7475381efc4544f
Fabio Montefuscolo <200063206+fabiomontefuscoloprotolabs@users.noreply.github.com>
2025-08-19 16:01:19 +0200

    feat: copy github permalink

---
+++ lua/plugins/gitlinker.lua
@@ -0,0 +1,12 @@
+return {
+  "ruifm/gitlinker.nvim",
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+  },
+  opts = {
+    mappings = "<leader>gy",
+    opts = {
+      print_url = true,
+    },
+  },
+}
