f527cd6d92772c110ce0a51df6bbc8a10f43e16d
Fabio Montefuscolo <fabio.montefuscolo@gmail.com>
2026-01-17 22:35:06 +0100

    chore: update gitlinker

--- lua/plugins/gitlinker.lua
+++ lua/plugins/gitlinker.lua
@@ -1,12 +1,9 @@
 return {
-  "ruifm/gitlinker.nvim",
-  dependencies = {
-    "nvim-lua/plenary.nvim",
-  },
-  opts = {
-    mappings = "<leader>gy",
-    opts = {
-      print_url = true,
-    },
+  'linrongbin16/gitlinker.nvim',
+  cmd = 'GitLink',
+  opts = {},
+  keys = {
+    { '<leader>gy', '<cmd>GitLink<cr>',  mode = { 'n', 'v' }, desc = 'Yank git link' },
+    { '<leader>gY', '<cmd>GitLink current_branch<cr>', mode = { 'n', 'v' }, desc = 'Yank git link' },
   },
 }
