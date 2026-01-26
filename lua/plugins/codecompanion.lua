97cee421b1d30a246e19214feaa81c49858e1877
Fabio Montefuscolo <fabio.montefuscolo@gmail.com>
2026-01-26 14:17:17 +0100

    chore: setting up codecompanion

--- lua/plugins/codecompanion.lua
+++ lua/plugins/codecompanion.lua
@@ -4,8 +4,41 @@ return {
     'ravitemer/mcphub.nvim',
   },
   opts = {
+    extensions = {
+      mcphub = {
+        callback = "mcphub.extensions.codecompanion",
+        opts = {
+          make_vars = true,
+          make_slash_commands = true,
+          show_result_in_chat = true
+        }
+      }
+    },
     interactions = {
       chat = {
+        tools = {
+          opts = {
+            auto_submit_errors = true,
+            auto_submit_success = true,
+            default_tools = {
+              'cmd_runner',
+              'create_file',
+              'delete_file',
+              'fetch_webpage',
+              'file_search',
+              'files',
+              'full_stack_dev',
+              'get_changed_files',
+              'grep_search',
+              'insert_edit_into_file',
+              'list_code_usages',
+              'memory',
+              'next_edit_suggestion',
+              'read_file',
+              'web_search',
+            }
+          },
+        },
         adapter = os.getenv 'ANTHROPIC_API_KEY' and 'anthropic' or 'copilot',
         model = 'claude-sonnet-4-20250514',
       },
